62f0382d7ebf60a1e4abaf30dbebc0f287ba0421
Israel Costa <icc@cin.ufpe.br>
2019-01-05 09:49:43 -0300

    Tela de login

---
+++ app/src/main/java/com/example/carlos/appclickshow/Activity/InitActivity.java
@@ -0,0 +1,15 @@
+package com.example.carlos.appclickshow.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.carlos.appclickshow.R;
+
+public class InitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init);
+    }
+}
